1bf88a995dde767e71ff656b30e253bdf236753b
RosieGrafSpee <proxima13922931@gmail.com>
2019-04-16 13:17:19 -0500

    a

--- FractalTreeLab/TreeComponent.java
+++ FractalTreeLab/TreeComponent.java
@@ -3,6 +3,7 @@ import javax.swing.JComponent;
 import java.awt.*;
 import javax.swing.JPanel;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.lang.Math.*;
 
 /**
@@ -13,37 +14,33 @@ import java.lang.Math.*;
  */
 public class TreeComponent
 {
-    private double l_deg, r_deg, br_size, scale;
+    private double l_deg, r_deg, orient, br_size, scale;
+    private Point2D.Double base;
 
     /**
      * Constructor for objects of class TreeComponent
      */
-    public TreeComponent(double l_deg, double r_deg, double scale, double br_size)
+    public TreeComponent(double l_deg, double r_deg, double orient, double br_size, double scale, Point2D.Double loc)
     {
         this.l_deg = l_deg;
         this.r_deg = r_deg;
+        this.orient = orient;
         this.br_size = br_size;
         this.scale = scale;
+        this.base = loc;
     }
     
     public void drawTree(Graphics2D g)
     {
-        TreeComponent temp = new TreeComponent(l_deg, r_deg, scale, br_size / scale);
-        if (this.scale >= 1)
+        Point2D.Double end = new Point2D.Double(base.getX() + br_size * Math.cos(orient), base.getY() + br_size * Math.sin(orient));
+        TreeComponent a = new TreeComponent(l_deg, r_deg, orient + l_deg, br_size / scale, scale, end);
+        TreeComponent b = new TreeComponent(l_deg, r_deg, orient - r_deg, br_size / scale, scale, end);
+        if (br_size > 1)
         {
-            temp.drawTree(g);
+            a.drawTree(g);
+            b.drawTree(g);
         }
         
-        
-    }
-    
-    public double getLeft()
-    {
-        return this.l_deg;
-    }
-    
-    public double getRight()
-    {
-        return this.r_deg;
+        g.drawLine((int)this.base.getX(), (int)this.base.getY(), (int)end.getX(), (int)end.getY());
     }
 }
